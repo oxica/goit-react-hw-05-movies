a3f695a23c82af99513a27f9e0adf7e07a4e891d
oxica <88445174+oxica@users.noreply.github.com>
2022-06-02 08:31:18 +0000

    Deploying to gh-pages from @ oxica/goit-react-hw-05-movies@5721a7f7586f74b96d557f835e93e388a0ebc7df 🚀

---
+++ static/js/NotFoundView.0192867c.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[558],{3438:function(e,t,n){n.r(t),n.d(t,{default:function(){return u}});var r=n(8931),o=n(184);function u(){var e=(0,r.k6)();return(0,o.jsxs)(o.Fragment,{children:[(0,o.jsx)("h2",{children:"404 Not Found "}),(0,o.jsx)("button",{type:"button",onClick:function(){e.push("/")},children:"Go home"})]})}}}]);
+//# sourceMappingURL=NotFoundView.0192867c.chunk.js.map
